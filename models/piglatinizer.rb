2917adc5ae27995cc57433d0dfcb998dec8682c9
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-16 02:21:34 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,11 +6,19 @@ class PigLatinizer
   end
 
   def piglatinize(word)
-    if self.is_vowel?(word)
-      word.concat('way')
-    else
-      word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
-    end
+    if !is_vowel?(word[0])
+     word = word + "w"
+
+   elsif is_vowel?(word[0]) && is_vowel?(word[1]) && is_vowel?(word[2])
+       word = word.slice(3..-1) + word.slice(0,3)
+
+     elsif is_vowel?(word[0]) && is_vowel?(word[1])
+       word = word.slice(2..-1) + word.slice(0,2)
+
+     else
+       word = word.slice(1..-1) + word.slice(0)
+     end
+     word << "ay"
   end
 
   def to_pig_latin(phrase)
